ebbbb4582cf4ca64e51ecc34dfd1129fe8ef9796
Thomas Waldmann <tw@waldmann-edv.de>
2026-03-08 19:44:12 +0100

    add setuptools_scm for versioning

--- src/cheapermac/main.py
+++ src/cheapermac/main.py
@@ -1,13 +1,17 @@
 import argparse
 import signal
 import multiprocessing
-from cheapermac.simulator import CheaperMac
 
+from cheapermac import __version__
+from cheapermac.simulator import CheaperMac
 
 def main():
     multiprocessing.freeze_support()
     parser = argparse.ArgumentParser(
         description="cheapermac - simulate a Mac with restricted resources."
+    )
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {__version__}"
     )
     parser.add_argument(
         "--memory", type=float, default=8.0, help="Memory limit in GB (default: 8.0 GB)"
